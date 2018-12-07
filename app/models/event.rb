a6c4611d18ecf39edefde9334721d72532f427aa
Christofer Flores <cristofer.flort@gmail.com>
2018-12-06 21:30:13 -0600

    Create the enrollment model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :admin_user
+
+  has_many :enrollments
+  has_many :students, through: :enrollments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_025223) do
+ActiveRecord::Schema.define(version: 2018_12_07_032518) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,17 @@ ActiveRecord::Schema.define(version: 2018_12_07_025223) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.boolean "attended"
+    t.datetime "attended_date"
+    t.integer "event_id"
+    t.integer "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_enrollments_on_event_id"
+    t.index ["student_id"], name: "index_enrollments_on_student_id"
   end
 
   create_table "events", force: :cascade do |t|
