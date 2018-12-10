290146ba636fed823f8c6ff986d4fcd0b73b8362
Christofer Flores <cristofer.flort@gmail.com>
2018-12-09 20:31:01 -0600

    Refactor the method for the image on the event model and also add the show view along with some comments on the code

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -16,11 +16,17 @@ ActiveAdmin.register Event, as: 'Eventos' do
     end
   end
 
-  # Index
+  # ================================================= START INDEX =========================================================
+  # _____ _   _ _____  ________   __
+  # |_   _| \ | |  __ \|  ____\ \ / /
+  #   | | |  \| | |  | | |__   \ V / 
+  #   | | | . ` | |  | |  __|   > <  
+  #  _| |_| |\  | |__| | |____ / . \ 
+  # |_____|_| \_|_____/|______/_/ \_\
   index do
     selectable_column
-    column "Imagen" do |evt|
-      link_to image_tag(evt.get_index_image, size: "200x150", alt: evt.name), admin_evento_path(evt)
+    column "Imagen" do |event|
+      link_to image_tag(event.get_transformed_image('w_200,h_150'), size: "200x150", alt: event.name), admin_evento_path(event)
     end
     column "Nombre del evento" do |event|
       link_to event.name, admin_evento_path(event)
@@ -30,8 +36,48 @@ ActiveAdmin.register Event, as: 'Eventos' do
     column 'Carrera', :career
     actions
   end
+  # ================================================= END INDEX =========================================================
 
-  # Form
+
+  # ================================================= START SHOW =========================================================
+  #  ____ ___ ____  _____ ____    _    ____      ___       ____  _   _  _____        __
+  # / ___|_ _|  _ \| ____| __ )  / \  |  _ \    ( _ )     / ___|| | | |/ _ \ \      / /
+  # \___ \| || | | |  _| |  _ \ / _ \ | |_) |   / _ \/\   \___ \| |_| | | | \ \ /\ / / 
+  #  ___) | || |_| | |___| |_) / ___ \|  _ <   | (_>  <    ___) |  _  | |_| |\ V  V /  
+  # |____/___|____/|_____|____/_/   \_\_| \_\   \___/\/   |____/|_| |_|\___/  \_/\_/  
+  show do
+    attributes_table do
+      row :name
+      row :career
+      row :location
+      row :start_date
+      row :end_date
+
+      panel 'Descripcion' do
+        attributes_table_for resource do
+          insert_tag(Arbre::HTML::Div) { content_tag(:span, raw(resource.description)) }
+        end
+      end
+    end
+  end
+  # ============ SIDEBAR ============
+  sidebar 'Imagen de Portada', only: :show do
+    attributes_table  do
+      row ' ' do |event|
+        image_tag(event.get_transformed_image('w_200,h_200'), size: "200x200", alt: event.name)
+      end
+    end
+  end
+  # ================================================= END SHOW =========================================================
+
+
+  # ================================================= START FORM =========================================================
+  # ______ ____  _____  __  __ 
+  # |  ____/ __ \|  __ \|  \/  |
+  # | |__ | |  | | |__) | \  / |
+  # |  __|| |  | |  _  /| |\/| |
+  # | |   | |__| | | \ \| |  | |
+  # |_|    \____/|_|  \_\_|  |_|
   form do |f|
     f.inputs do
       f.input :name, label: 'Nombre del evento'
@@ -53,10 +99,19 @@ ActiveAdmin.register Event, as: 'Eventos' do
     end
     f.actions
   end
+  # ================================================== END FORM ==========================================================
+  
 
-  # Filters
+  # ================================================= START FILTERS ======================================================
+  #  ______ _____ _   _______ ______ _____   _____ 
+  # |  ____|_   _| | |__   __|  ____|  __ \ / ____|
+  # | |__    | | | |    | |  | |__  | |__) | (___  
+  # |  __|   | | | |    | |  |  __| |  _  / \___ \ 
+  # | |     _| |_| |____| |  | |____| | \ \ ____) |
+  # |_|    |_____|______|_|  |______|_|  \_\_____/
   filter :name_cont, label: 'Nombre'
   filter :career_cont, label: 'Carrera a la que aplica'
   filter :start_date, label: 'Fecha de inicio'
+  # ================================================== END FILTERS ========================================================
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,10 @@ class Event < ApplicationRecord
   has_many :enrollments
   has_many :students, through: :enrollments
 
-  def get_index_image
-    if cover
+  def get_transformed_image(transformation)
+     if cover
       img_parts = cover.split('upload')
-      img_parts[0] + '/upload/w_200,h_150,q_auto,f_auto' + img_parts[1]
+      img_parts[0] + '/upload/q_auto,f_auto,' + transformation + ',' + img_parts[1]
     else
       'https://via.placeholder.com/150?text=sin imagen'
     end
