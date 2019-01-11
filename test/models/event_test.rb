20ddcc5a8245ac034640d247ac156f115c845f7b
Christofer Flores <christofer@michelada.io>
2019-01-11 17:55:35 -0600

    Clean up the test file

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
 
-  def before_setup
-    super
+  def setup
     @event = Event.new(
       name: 'Testinsg', 
       career: 'Resdes', 
@@ -14,48 +13,36 @@ class EventTest < ActiveSupport::TestCase
   end
 
   # Validations
-  it 'Not save if any of the required fields are missing' do
-    test 'Event: should not save if not name' do
-      @event.name = nil
-      assert_not @event.save, 'Event saved whitout name'
-    end
-
-    test 'Event: should not save if not career' do
-      @event.career = nil
-      assert_not @event.save, 'Event saved whitout career'
-    end
-
-    test 'Event: should not save if not description' do
-      @event.description = nil
-      assert_not @event.save, 'Event saved whitout description'
-    end
-
-    test 'Event: should not save if not location' do
-      @event.location = nil
-      assert_not @event.save, 'Event saved whitout location'
-    end
-
-    test 'Event: should not save if not start_date' do
-      @event.start_date = nil
-      assert_not @event.save, 'Event saved whitout start_date'
-    end
-
-    test 'Event: should not save if not end_date' do
-      @event.end_date = nil
-      assert_not @event.save, 'Event saved whitout end_date'
-    end
-
+  test 'Event: should not save if not name' do
+    @event.name = nil
+    assert_not @event.save, 'Event saved whitout name'
   end
 
-  
-  # should "recommend 2 highest ranked, in stock movies unwatched by user" do
-  #   user    = Factory(:user)
-  #   top_out = Factory(:movie, :ranking => 100, :in_stock => false)
-  #   top_in  = Factory(:movie, :ranking => 95,  :in_stock => true)
-  #   next_in = Factory(:movie, :ranking => 90,  :in_stock => true)
-  #   watched = Factory(:movie, :ranking => 100, :in_stock => true)
-  #   Factory(:viewing, :user => user, :movie => watched)
+  test 'Event: should not save if not career' do
+    @event.career = nil
+    assert_not @event.save, 'Event saved whitout career'
+  end
 
-  #   assert_equal [top_in, next_in], Movie.recommended_for(user, 2)
-  # end
+  test 'Event: should not save if not description' do
+    @event.description = nil
+    assert_not @event.save, 'Event saved whitout description'
+  end
+
+  test 'Event: should not save if not location' do
+    @event.location = nil
+    assert_not @event.save, 'Event saved whitout location'
+  end
+
+  test 'Event: should not save if not start_date' do
+    @event.start_date = nil
+    assert_not @event.save, 'Event saved whitout start_date'
+  end
+
+  test 'Event: should not save if not end_date' do
+    @event.end_date = nil
+    assert_not @event.save, 'Event saved whitout end_date'
+  end
+
+  # Scopes
+
 end
